361b4b0a7f8312b6b8538475fd8c45759d039d4f
jelhamm <elhamjafari200312@gmail.com>
2024-04-04 02:44:49 +0430

    Add CustomFunctions Class

--- Source Code/Python Source File/Deep_Learning_for_the_Gaussian_Wiretap_Channel.py
+++ Source Code/Python Source File/Deep_Learning_for_the_Gaussian_Wiretap_Channel.py
@@ -43,3 +43,21 @@ messages = np.random.randint(M, size=SAMPLE_SIZE)                            # G
 one_hot_encoder = OneHotEncoder(sparse_output=False, categories=[range(M)])  # Encode messages using OneHotEncoder
 data_oneH = one_hot_encoder.fit_transform(messages.reshape(-1, 1))
  
+class CustomFunctions:
+    @staticmethod
+    def snr_to_noise(snrdb):                    # Convert SNR in dB to noise standard deviation
+        snr = 10**(snrdb/10)
+        noise_std = 1/np.sqrt(2*snr)
+        return noise_std
+
+    @staticmethod
+    def B_Ber(input_msg, msg):                  # Calculate bit error rate from input and predicted messages
+        pred_error = tf.not_equal(tf.argmax(msg, 1), tf.argmax(input_msg, 1))
+        bber = tf.reduce_mean(tf.cast(pred_error, tf.float32))
+        return bber
+
+    @staticmethod 
+    def random_batch(X, batch_size=32):         # Get a random batch of data from X
+        idx = np.random.randint(len(X), size=batch_size)
+        return X[idx]     
+    
